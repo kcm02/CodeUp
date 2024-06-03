6e1ba64b922e45c34542fbdc30591df2a4b923d8
kcm02 <112393082+kcm02@users.noreply.github.com>
2024-06-03 14:50:13 +0900

    Add files via upload

---
+++ 6043_[기초-산술연산] 실수 2개 입력받아 나눈 결과 계산하기(py).py
@@ -0,0 +1,2 @@
+﻿f1, f2 = input().split(' ')
+print(format(float(f1)/float(f2),'.3f'))
---
+++ 6045_[기초-산술연산] 정수 3개 입력받아 합과 평균 출력하기(설명)(py).py
@@ -0,0 +1,6 @@
+﻿a,b,c = input().split(' ')
+a = int(a)
+b = int(b)
+c = int(c)
+
+print(a+b+c,format((a+b+c)/3,'.2f'),sep=' ')
