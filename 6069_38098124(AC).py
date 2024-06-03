52f6c409d4d363f9a34d014c25f322ccb230ac4e
kcm02 <112393082+kcm02@users.noreply.github.com>
2024-06-03 16:32:33 +0900

    Add files via upload

---
+++ 6069_38098124(AC).py
@@ -0,0 +1,11 @@
+﻿w = input()
+if w == 'A':
+    print('best!!!')
+elif w == 'B':
+    print('good!!')
+elif w == 'C':
+    print('run!')
+elif w == 'D':
+    print('slowly~')
+else:
+    print('what?')
---
+++ 6070_38098314(AC).py
@@ -0,0 +1,10 @@
+﻿m = int(input())
+
+if m == 12 or m <= 2:
+    print('winter')
+elif m <= 5:
+    print('spring')
+elif m <= 8:
+    print('summer')
+else:
+    print('fall')
