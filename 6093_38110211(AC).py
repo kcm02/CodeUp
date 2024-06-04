c6ade8ee76ebb8bb309460fbfe00a05fb9fb7f0d
kcm02 <112393082+kcm02@users.noreply.github.com>
2024-06-04 16:08:02 +0900

    Add files via upload

---
+++ 6093_38110211(AC).py
@@ -0,0 +1,6 @@
+﻿n = int(input())
+n_list = list(map(int,input().split()))
+n_list.reverse()
+
+for i in n_list:
+    print(i, end=' ')
---
+++ 6094_38110236(AC).py
@@ -0,0 +1,5 @@
+﻿n = int(input())
+n_list = list(map(int,input().split()))
+min_num = min(n_list)
+
+print(min_num)
