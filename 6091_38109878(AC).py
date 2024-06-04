9c40becafff8531680790bbf5992942e30e9afd7
kcm02 <112393082+kcm02@users.noreply.github.com>
2024-06-04 16:07:26 +0900

    Add files via upload

---
+++ 6091_38109878(AC).py
@@ -0,0 +1,7 @@
+﻿a,b,c = map(int,input().split())
+n = max(a,b,c)
+
+while not (n % a == 0 and n % b == 0 and n % c == 0):
+    n += 1
+
+print(n)
---
+++ 6092_38110103(AC).py
@@ -0,0 +1,12 @@
+﻿n = int(input())
+list = list(map(int,input().split()))
+student = [i for i in range(1,24)]
+count = [0 for _ in range(23)]
+
+for i in list:
+    for j in student:
+        if i == j:
+            count[j-1] += 1
+
+for i in count:
+    print(i, end=' ')
